49200a089939db4734cbee939e2c986009e2b0b9
Malh1n <58389533+Malh1n@users.noreply.github.com>
2021-06-10 01:41:18 +0300

    #1-#6 done

--- 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
@@ -7,33 +7,33 @@ import java.util.List;
 
 public class CarService {
 
-    public List<Car> createCarList(int carCount) {
+    public List<Car> createCarList(int requestCount) {
         List<Car> createdList = new ArrayList<>();
         createdList.add(new Car("Audi", 100, "White"));
         createdList.add(new Car("BMW", 3, "Black"));
         createdList.add(new Car("Ford", 12, "Green"));
         createdList.add(new Car("Ferrari", 666, "Orange"));
         createdList.add(new Car("Dodge", 55, "Grey"));
-        List<Car> result = new ArrayList<>();
-        for (int i = 0; i < carCount; i++) {
-            result.add(createdList.get(i));
+        List<Car> forRequestList = new ArrayList<>();
+        for (int i = 0; i < requestCount; i++) {
+            forRequestList.add(createdList.get(i));
         }
-        return result;
+        return forRequestList;
     }
 
     public List<Car> getCarList(String count) {
-        List<Car> carList;
+        List<Car> requestList;
         CarService carService = new CarService();
         if (count == null) {
             return carService.createCarList(5);
         } else {
             int cars = Integer.parseInt(count);
             if (cars >= 5) {
-                carList = carService.createCarList(5);
+                requestList = carService.createCarList(5);
             } else {
-                carList = carService.createCarList(cars);
+                requestList = carService.createCarList(cars);
             }
         }
-        return carList;
+        return requestList;
     }
 }
